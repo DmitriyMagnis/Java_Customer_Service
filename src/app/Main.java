85e00afdaa2a4cf1ac1e70f578057d7a7f201143
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-11-02 14:43:24 +0100

    ADD/basic Customer usage

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Customer Service App");
+
+        Customer customer = getCustomer(getData());
+        String output = "Customer: " + customer.getName() +
+                ",\n phone " + customer.getPhone();
+        getOutput(output);
+    }
+
+    private static String[] getData() {
+        return new String[]{"Dmytro", "+380682991803"};
+    }
+
+    private static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
